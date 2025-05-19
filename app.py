9a6468388ad9d44d7afeabd218edbf099a80f850
Janet35 <janetl35@nycstudents.net>
2025-05-19 10:13:39 -0400

    commit

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
-from flask import Flask, render_template
+import http.client
 
-app = Flask(__name__)
+host = 'jooble.org'
+key = '<YOUR_API_KEY>'
 
-movies = [
-    {"id": 1, "title": "Avengers: Endgame", "price": 12},
-    {"id": 2, "title": "Spider-Man: No Way Home", "price": 10},
-    {"id": 3, "title": "Inception", "price": 8}
-]
+connection = http.client.HTTPConnection(host)
+#request headers
+headers = {"Content-type": "application/json"}
+#json query
+body = '{ "keywords": "it", "location": "Bern"}'
+connection.request('POST','/api/' + key, body, headers)
+response = connection.getresponse()
+print(response.status, response.reason)
+print(response.read())
 
-@app.route('/')
-def home():
-    return render_template('index.html', movies=movies)
-
-app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
